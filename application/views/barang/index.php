2e2509cfbd8a83658b09db94dc82cd67c91a5f05
didikkoetoe <danieldidikprabowo@gmail.com>
2021-08-14 18:01:46 +0700

    Menambahkan btn tambah barang

--- application/views/barang/index.php
+++ application/views/barang/index.php
@@ -1,6 +1,7 @@
 <div class="container mt-3">
 	<div class="row mt-3">
 		<div class="col-md-6">
+			<a href="<?= base_url(); ?>Barang/tambah" class="btn btn-primary float-end">Tambah Barang</a>
 			<h2 class="text-center">Daftar Barang</h2>
 			<ul class="list-group">
 				<?php foreach($barang as $brg): ?>
