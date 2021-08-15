fff0553a343af3636d5e4dda0162873fd23cb430
didikkoetoe <danieldidikprabowo@gmail.com>
2021-08-15 08:35:35 +0700

    Menambahkan flasher

--- application/views/barang/index.php
+++ application/views/barang/index.php
@@ -1,4 +1,16 @@
 <div class="container mt-3">
+
+	<?php if ($this->session->flashdata('flash')): ?>
+		<div class="row mt-3">
+			<div class="col-md-6">
+				<div class="alert alert-warning alert-dismissible fade show" role="alert">
+					Data barang <strong><?= $this->session->flashdata('flash'); ?></strong> di tambahkan.
+					<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+
 	<div class="row mt-3">
 		<div class="col-md-6">
 			<a href="<?= base_url(); ?>Barang/tambah" class="btn btn-primary float-end">Tambah Barang</a>
